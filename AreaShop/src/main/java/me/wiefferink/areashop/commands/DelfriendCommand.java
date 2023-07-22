f2152f71e511eb43e27ed299f1cc2b9ffb4c2887
Austin Westphal <56894247+Whoops-Lambo@users.noreply.github.com>
2023-07-22 17:47:50 -0400

    Update DelfriendCommand.java

--- AreaShop/src/main/java/me/wiefferink/areashop/commands/DelfriendCommand.java
+++ AreaShop/src/main/java/me/wiefferink/areashop/commands/DelfriendCommand.java
@@ -1,9 +1,9 @@
-package me.wiefferink.areashop.commands;
+package me.devaustin.deephomes.commands;
 
-import me.wiefferink.areashop.regions.BuyRegion;
-import me.wiefferink.areashop.regions.GeneralRegion;
-import me.wiefferink.areashop.regions.RentRegion;
-import me.wiefferink.areashop.tools.Utils;
+import me.devaustin.deephomes.regions.BuyRegion;
+import me.devaustin.deephomes.regions.GeneralRegion;
+import me.devaustin.deephomes.regions.RentRegion;
+import me.devaustin.deephomes.tools.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
@@ -12,16 +12,16 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DelfriendCommand extends CommandAreaShop {
+public class DelfriendCommand extends CommandDeepHomes {
 
 	@Override
 	public String getCommandStart() {
-		return "areashop delfriend";
+		return "deephomes delfriend";
 	}
 
 	@Override
 	public String getHelp(CommandSender target) {
-		if(target.hasPermission("areashop.delfriendall") || target.hasPermission("areashop.delfriend")) {
+		if(target.hasPermission("deephomes.delfriendall") || target.hasPermission("deephomes.delfriend")) {
 			return "help-delFriend";
 		}
 		return null;
@@ -34,12 +34,12 @@ public class DelfriendCommand extends CommandAreaShop {
 	 * @return true if the person can remove friends, otherwise false
 	 */
 	public static boolean canUse(CommandSender person, GeneralRegion region) {
-		if(person.hasPermission("areashop.delfriendall")) {
+		if(person.hasPermission("deephomes.delfriendall")) {
 			return true;
 		}
 		if(person instanceof Player) {
 			Player player = (Player)person;
-			return region.isOwner(player) && player.hasPermission("areashop.delfriend");
+			return region.isOwner(player) && player.hasPermission("deephomes.delfriend");
 		}
 		return false;
 	}
@@ -47,7 +47,7 @@ public class DelfriendCommand extends CommandAreaShop {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		if(!sender.hasPermission("areashop.delfriend") && !sender.hasPermission("areashop.delfriendall")) {
+		if(!sender.hasPermission("deephomes.delfriend") && !sender.hasPermission("deephomes.delfriendall")) {
 			plugin.message(sender, "delfriend-noPermission");
 			return;
 		}
@@ -80,7 +80,7 @@ public class DelfriendCommand extends CommandAreaShop {
 				return;
 			}
 		}
-		if(sender.hasPermission("areashop.delfriendall")) {
+		if(sender.hasPermission("deephomes.delfriendall")) {
 			if((region instanceof RentRegion && !((RentRegion)region).isRented())
 					|| (region instanceof BuyRegion && !((BuyRegion)region).isSold())) {
 				plugin.message(sender, "delfriend-noOwner", region);
@@ -96,7 +96,7 @@ public class DelfriendCommand extends CommandAreaShop {
 				plugin.message(sender, "delfriend-successOther", friend.getName(), region);
 			}
 		} else {
-			if(sender.hasPermission("areashop.delfriend") && sender instanceof Player) {
+			if(sender.hasPermission("deephomes.delfriend") && sender instanceof Player) {
 				if(region.isOwner((Player)sender)) {
 					OfflinePlayer friend = Bukkit.getOfflinePlayer(args[1]);
 					if(!region.getFriendsFeature().getFriends().contains(friend.getUniqueId())) {
