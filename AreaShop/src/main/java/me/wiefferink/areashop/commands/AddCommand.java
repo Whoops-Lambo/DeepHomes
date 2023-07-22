7d01894db2966e44589b60295ddafa896065fb08
Austin Westphal <56894247+Whoops-Lambo@users.noreply.github.com>
2023-07-22 17:17:43 -0400

    Update AddCommand.java

--- AreaShop/src/main/java/me/wiefferink/areashop/commands/AddCommand.java
+++ AreaShop/src/main/java/me/wiefferink/areashop/commands/AddCommand.java
@@ -296,16 +296,16 @@ public class AddCommand extends CommandDeepHomes {
 	public List<String> getTabCompleteList(int toComplete, String[] start, CommandSender sender) {
 		List<String> result = new ArrayList<>();
 		if(toComplete == 2) {
-			if(sender.hasPermission("areashop.createrent")) {
+			if(sender.hasPermission("deephomes.createrent")) {
 				result.add("rent");
 			}
-			if(sender.hasPermission("areashop.createbuy")) {
+			if(sender.hasPermission("deephomes.createbuy")) {
 				result.add("buy");
 			}
 		} else if(toComplete == 3) {
 			if(sender instanceof Player) {
 				Player player = (Player)sender;
-				if(sender.hasPermission("areashop.createrent") || sender.hasPermission("areashop.createbuy")) {
+				if(sender.hasPermission("deephomes.createrent") || sender.hasPermission("deephomes.createbuy")) {
 					for(ProtectedRegion region : plugin.getRegionManager(player.getWorld()).getRegions().values()) {
 						result.add(region.getId());
 					}
